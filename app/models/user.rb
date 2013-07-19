4b8c91b84a2da97b7f05561d2b1b5681fa4286bf
upinetree <essequake@gmail.com>
2013-07-19 21:39:16 +0900

    refactor email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = email.downcase }
+  before_save { email.downcase! }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :name,     presence: true, length: { maximum: 50 }
   validates :email,    presence: true,
                        format: { with: VALID_EMAIL_REGEX },
