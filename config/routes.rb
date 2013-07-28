dbc320cf1c1c5821d17ea64e9ab62e7ca0ec34d1
upinetree <essequake@gmail.com>
2013-07-28 23:22:35 +0900

    add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
+  resources :users
 
   root to: 'static_pages#home'
 
-  match '/signup',  to: 'users#new'
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
 
 
   # The priority is based upon order of creation:
